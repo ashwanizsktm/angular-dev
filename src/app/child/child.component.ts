4d807e4a9c278c28e3192b36894238ee0b2281b5
ashwanizsktm <ashwanizskt@gmail.com>
2021-11-18 10:18:07 +0530

    5 files modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,12 +6,30 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
-
+  theItem = [];
+   tempArr = [];
   @Output() childMsg = new EventEmitter();
+
+  @Input() set items(item) {
+    // console.log(item);
+    this.tempArr = item;
+    this.theItem = this.tempArr.concat(" hey there")
+    
+    // itemsArr.filter(item => item != 'item 3')
+    
+    // this.theItem = itemsArr;
+    
+    // this.theItem = item;
+    // let filteredData = [];
+    // this.
+    // this.theItem.filter((item) => {
+  }
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // console.log(this.items);
+  }
 
   sendingData(value) {
     this.childMsg.emit(value.innerText);
