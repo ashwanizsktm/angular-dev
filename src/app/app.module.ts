985a42a953b9637248f81e8a53dd9ec8c51323f4
ashwanizsktm <ashwanizskt@gmail.com>
2021-12-27 10:56:14 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { ValueRoundoffPipe } from '../value-roundoff.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ChildComponent],
+  declarations: [AppComponent, ChildComponent, ValueRoundoffPipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
